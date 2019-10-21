d47bf067c44aa2c85f5a577aa54a17a64f134b92
kimbs <vlakd0711@naver.com>
2019-10-21 13:35:59 +0900

    add init department bean

--- src/main/kotlin/org/kimbs/demo/DemoApplication.kt
+++ src/main/kotlin/org/kimbs/demo/DemoApplication.kt
@@ -1,13 +1,21 @@
 package org.kimbs.demo
 
-import org.slf4j.LoggerFactory
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
 class DemoApplication {
-    private val log = LoggerFactory.getLogger(DemoApplication::class.java);
 
+    @Bean
+    fun init(departmentRepository: DepartmentRepository) = CommandLineRunner {
+        departmentRepository.save(Department(1, "Solution", "201"));
+        departmentRepository.save(Department(2, "Process", "202"));
+        departmentRepository.save(Department(3, "Engineering", "203"));
+        departmentRepository.save(Department(4, "Personnel", "204"));
+        departmentRepository.save(Department(5, "Marketing", "205"));
+    }
 }
 
 fun main(args: Array<String>) {
